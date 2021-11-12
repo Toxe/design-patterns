9c28df6cfe8f1bb4bbf8ceca6df16bbfb786588d
Toxe <toxe@toxe.net>
2021-11-12 09:01:12 +0100

    Clang-cl warning options

--- cmake/DefaultCompilerOptionsAndWarnings.cmake
+++ cmake/DefaultCompilerOptionsAndWarnings.cmake
@@ -1,41 +1,50 @@
 # Define default compiler options and warnings for Clang, GCC and MSVC.
 
-# shared options between Clang and GCC
-set(COMMON_CLANG_AND_GCC_WARNINGS
-    -Wall
-    -Wextra
-    -pedantic
-    -Wconversion
-    -Wsign-conversion
-    -Wfloat-equal
-    -Wold-style-cast
-    -Wshadow
-    -Wunused
-)
+# shared options between Clang, Clang-cl and GCC
+set(SHARED_CLANG_AND_CLANG_CL_AND_GCC_OPTIONS
+        -Wextra
+        -Wconversion
+        -Wsign-conversion
+        -Wfloat-equal
+        -Wold-style-cast
+        -Wshadow
+        -Wunused)
 
 # Clang specific options
 set(DEFAULT_CLANG_OPTIONS
-    ${COMMON_CLANG_AND_GCC_WARNINGS}
-    -Wmost
-    -fcolor-diagnostics
-)
+        ${SHARED_CLANG_AND_CLANG_CL_AND_GCC_OPTIONS}
+        -Wall
+        -Wmost
+        -pedantic
+        -fcolor-diagnostics)
 
 # GCC specific options
 set(DEFAULT_GCC_OPTIONS
-    ${COMMON_CLANG_AND_GCC_WARNINGS}
-    -fdiagnostics-color
-)
+        ${SHARED_CLANG_AND_CLANG_CL_AND_GCC_OPTIONS}
+        -Wall
+        -pedantic
+        -fdiagnostics-color)
 
 # MSVC specific options
 set(DEFAULT_MSVC_OPTIONS
-    /W4
-)
+        /W4)
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+# Clang-cl specific options (Clang + MSVC)
+set(DEFAULT_CLANG_CL_OPTIONS
+        ${SHARED_CLANG_AND_CLANG_CL_AND_GCC_OPTIONS}
+        ${DEFAULT_MSVC_OPTIONS})
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+    # Clang
     set(DEFAULT_COMPILER_OPTIONS_AND_WARNINGS ${DEFAULT_CLANG_OPTIONS})
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+    # Clang-cl
+    set(DEFAULT_COMPILER_OPTIONS_AND_WARNINGS ${DEFAULT_CLANG_CL_OPTIONS})
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    # GCC
     set(DEFAULT_COMPILER_OPTIONS_AND_WARNINGS ${DEFAULT_GCC_OPTIONS})
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    # MSVC
     string(REGEX REPLACE " /W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     set(DEFAULT_COMPILER_OPTIONS_AND_WARNINGS ${DEFAULT_MSVC_OPTIONS})
-endif()
+endif ()
