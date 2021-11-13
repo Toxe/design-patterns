fbfca8fe5c790616c5c91774f5d4f5f5c9759ed9
Toxe <toxe@toxe.net>
2021-11-13 13:06:53 +0100

    Remove color output

--- src/bridge/concrete_implementors/concrete_implementor1.cpp
+++ src/bridge/concrete_implementors/concrete_implementor1.cpp
@@ -1,11 +1,10 @@
 #include "concrete_implementor1.h"
 
-#include <fmt/color.h>
 #include <fmt/core.h>
 
 void ConcreteImplementor1::print_value(const int value)
 {
-    fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::orange), "{}", value);
+    fmt::print("{}", value);
 }
 
 void ConcreteImplementor1::print_delimiter()
--- src/bridge/concrete_implementors/concrete_implementor2.cpp
+++ src/bridge/concrete_implementors/concrete_implementor2.cpp
@@ -1,11 +1,10 @@
 #include "concrete_implementor2.h"
 
-#include <fmt/color.h>
 #include <fmt/core.h>
 
 void ConcreteImplementor2::print_value(const int value)
 {
-    fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::yellow), "<{}>", value);
+    fmt::print("<{}>", value);
 }
 
 void ConcreteImplementor2::print_delimiter()
